69b0672b8dc6590792848eeb2ae71ed89ec65308
JuanCarvajalD <125924849+JuanCarvajalD@users.noreply.github.com>
2023-03-12 23:11:54 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -143,20 +143,153 @@ def pregunta_01():
 
 
 def pregunta_02():
-    """
-    Retorne la cantidad de registros por cada letra de la primera columna como la lista
-    de tuplas (letra, cantidad), ordendas alfabéticamente.
+    with open('/Datos.txt', 'r') as archivo:
+         datos = archivo.read()
+    filas = datos.split('\n') 
+    fila0 = filas[0]
+    columna0 = fila0.split() 
+    D1 = (columna0[0]) 
+    fila1 = filas[1]
+    columna1 = fila1.split() 
+    D2 = (columna1[0]) 
+    fila2 = filas[2]
+    columna2 = fila2.split() 
+    D3 = (columna2[0]) 
+    fila3 = filas[3]
+    columna3 = fila3.split() 
+    D4 = (columna3[0])
+    fila4 = filas[4]
+    columna4 = fila4.split() 
+    D5 = (columna4[0])
+    fila5 = filas[5]
+    columna5 = fila5.split() 
+    D6 = (columna5[0])
+    fila6 = filas[6]
+    columna6 = fila6.split() 
+    D7 = (columna6[0])
+    fila7 = filas[7]
+    columna7 = fila7.split() 
+    D8 = (columna7[0])
+    fila8 = filas[8]
+    columna8 = fila8.split() 
+    D9 = (columna8[0])
+    fila9 = filas[9]
+    columna9 = fila9.split() 
+    D10 = (columna9[0])
+    fila10 = filas[10]
+    columna10 = fila10.split() 
+    D11 = (columna10[0])
+    fila11 = filas[11]
+    columna11 = fila11.split() 
+    D12 = (columna11[0])
+    fila12 = filas[12]
+    columna12 = fila12.split() 
+    D13 = (columna12[0])
+    fila13 = filas[13]
+    columna13 = fila13.split() 
+    D14 = (columna13[0])
+    fila14 = filas[14]
+    columna14 = fila14.split() 
+    D15 = (columna14[0])
+    fila15 = filas[15]
+    columna15 = fila15.split() 
+    D16 = (columna15[0])
+    fila16 = filas[16]
+    columna16 = fila16.split() 
+    D17 = (columna16[0])
+    fila17 = filas[17]
+    columna17 = fila17.split() 
+    D18 = (columna17[0])
+    fila18 = filas[18]
+    columna18 = fila18.split() 
+    D19 = (columna18[0])
+    fila19 = filas[19]
+    columna19 = fila19.split() 
+    D20 = (columna19[0])
+    fila20 = filas[20]
+    columna20 = fila20.split() 
+    D21 = (columna20[0])
+    fila21 = filas[21]
+    columna21 = fila21.split() 
+    D22 = (columna21[0])
+    fila22 = filas[22]
+    columna22 = fila22.split() 
+    D23 = (columna22[0])
+    fila23 = filas[23]
+    columna23 = fila23.split() 
+    D24 = (columna23[0])
+    fila24 = filas[24]
+    columna24 = fila24.split() 
+    D25 = (columna24[0])
+    fila25 = filas[25]
+    columna25 = fila25.split() 
+    D26 = (columna25[0])
+    fila26 = filas[26]
+    columna26 = fila26.split() 
+    D27 = (columna26[0])
+    fila27 = filas[27]
+    columna27 = fila27.split() 
+    D28 = (columna27[0])
+    fila28 = filas[28]
+    columna28 = fila28.split() 
+    D29 = (columna28[0])
+    fila29 = filas[29]
+    columna29 = fila29.split() 
+    D30 = (columna29[0])
+    fila30 = filas[30]
+    columna30 = fila30.split() 
+    D31 = (columna30[0])
+    fila31 = filas[31]
+    columna31 = fila31.split() 
+    D32 = (columna31[0])
+    fila32 = filas[32]
+    columna32 = fila32.split() 
+    D33 = (columna32[0])
+    fila33 = filas[33]
+    columna33 = fila33.split() 
+    D34 = (columna33[0])
+    fila34 = filas[34]
+    columna34 = fila34.split() 
+    D35 = (columna34[0])
+    fila35 = filas[35]
+    columna35 = fila35.split() 
+    D36 = (columna35[0])
+    fila36 = filas[36]
+    columna36 = fila36.split() 
+    D37 = (columna36[0])
+    fila37 = filas[37]
+    columna37 = fila37.split() 
+    D38 = (columna37[0])
+    fila38 = filas[38]
+    columna38 = fila38.split() 
+    D39 = (columna38[0])
+    fila39 = filas[39]
+    columna39 = fila39.split() 
+    D40 = (columna39[0])
 
-    Rta/
-    [
-        ("A", 8),
-        ("B", 7),
-        ("C", 5),
-        ("D", 6),
-        ("E", 14),
-    ]
+    X = [D1,D2,D3,D4,D5,D6,D7,D8,D9,D10,D11,D12,D13,D14,D15,D16,D17,D18,D19,D20,D21,D22,D23,D24,D25,D26,D27,D28,D29,D30,D31,D32,D33,D34,D35,D36,D37,D38,D39,D40]
+    contadorA = 0
+    contadorB = 0
+    contadorC = 0
+    contadorD = 0
+    contadorE = 0
+    for i in X:
+      if i == "A":
+        contadorA = contadorA + 1
+      elif i == "B":
+        contadorB = contadorB + 1
+      elif i == "C":
+        contadorC = contadorC + 1
+      elif i == "D":
+        contadorD = contadorD + 1  
+      else:
+        contadorE = contadorE + 1
 
-    """
+    print(("A", contadorA) )
+    print(("B", contadorB) )
+    print(("C", contadorC) )
+    print(("D", contadorD) )
+    print(("E", contadorE) )
     return
 
 
